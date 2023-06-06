40efac4079a34d688cfd5c968029526bda011eff
Andreas Weitzel <andi.weitzel@gmx.de>
2023-06-06 10:01:38 +0200

    commit on fwz

--- LearningProject/DomainSize/CalcCorrelationFunction.cpp
+++ LearningProject/DomainSize/CalcCorrelationFunction.cpp
@@ -58,7 +58,7 @@ int main() {
 
     // okay we need a system that calculates the correlation func for every file or at least for every directory
     // path of the root where we have multiple directories with multiple csv for every Temp
-    string rootpath = "../../Generated content/Adaptive Stepsize 2/";
+    string rootpath = "../../Generated content/Full Scan/";
     // now we need a function that returns the directories? but then we need to watch out that we dont have "plots"
     // directory there
     vector<fs::path> temp_directories = list_dir_paths(rootpath);
