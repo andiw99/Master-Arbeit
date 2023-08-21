184405c43545c93684a3d97b1156dbcd31cdbfd7
andi <andi.weitzel@gmx.de>
2023-08-21 12:47:42 +0200

    added lrun

---
+++ CudaProject/lrun.sh
@@ -0,0 +1,25 @@
+module load gcc/10.2.0
+module load cuda/12.1
+module load openmpi/4.1.1
+module load fftw/3.3.10-ompi411
+
+# Get the input and output file names from the command line arguments
+input_file=${1:-"main.cu"}
+output_file=${2:-"main_script"}
+
+# Compile the cuda file with nvcc
+nvcc -I /home/weitze73/Code/boost_1_82_0 -std=c++17  $input_file -o $output_file
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+    # run the simulation
+    # save the output in variable output
+    output=$(./$output_file | tee /dev/tty)
+    # cut only the last line, which is the save path
+    rootpath=$(echo "$output" | tail -1)
+    # run calc correlation function with the path as input variable
+    # remember to call it with argument since otherwise the hardcoded path is used
+    # make the plots
+else
+    echo "Compilation failed"
+fi
