b0b60dede626e5b4676e51889460fb6377c462e0
andi <andi.weitzel@gmx.de>
2023-08-26 18:12:37 +0200

    proper numbering of csv files

--- CudaProject/Simulation.cuh
+++ CudaProject/Simulation.cuh
@@ -124,7 +124,7 @@ public:
             stepper = create_stepper<state_type, alg, oper, sys, double, double, stepper_type>(paras);
             State_initializer = create_state_initializer<state_type>((int)paras["random"], paras, simulation_path);
         }
-        run(repeat_nr - runs + run_count);
+        run(run_count + 1);
         if(runs > 0) {
             repeat(runs-1);
         } else {
